7997e9d8263585a9bd55d29d867f0e75ee2a4be3
Alex Pedersen <hello@alexpedersen.dev>
2024-05-23 22:30:50 +0200

    chore: Add useSwitchChain hook to Account component

--- src/components/Account.tsx
+++ src/components/Account.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { config } from '@/wagmi'
-import { useAccount, useConnect, useDisconnect, useBalance } from 'wagmi'
+import { useAccount, useConnect, useDisconnect, useBalance, useSwitchChain } from 'wagmi'
 import { formatUnits } from 'viem'
 
 function Account() {
@@ -12,24 +12,40 @@ function Account() {
     address: account.address,
     config
   })
+  const { chains, switchChain, isPending } = useSwitchChain()
 
   return (
     <>
       <div>
         <h2>Account</h2>
-        <div>
-          status: {account.status}
-          <hr />
-          addresses: {JSON.stringify(account.addresses)}
-          <hr />
-          chainId: {account.chainId}
-          <hr />
-          balance: {data && formatUnits(data?.value, 18)}
-        </div>
+        status: {account.status}
+        <hr />
         {account.status === 'connected' ? (
-          <button type="button" onClick={() => disconnect()}>
-            Disconnect
-          </button>
+          <div>
+            {chains.map((chain) => {
+              if (chain.id === account.chainId) {
+                return (
+                  <button className='p-2 text-left' key={chain.id} disabled>
+                    {chain.name} (active)
+                  </button>
+                )
+              } else {
+                return (
+                  <button className='p-2 text-left' key={chain.id} onClick={() => switchChain({ chainId: chain.id })}>
+                    Switch to {chain.name}
+                    {isPending && ' (switching)'}
+                  </button>
+                )
+              }
+            })}
+            <button type="button" onClick={() => disconnect()}>
+              Disconnect
+            </button>
+            <hr />
+            addresses: {JSON.stringify(account.addresses)}
+            <hr />
+            balance: {data && formatUnits(data?.value, 18)}
+          </div>
         ) : (
           <div>
             <h3>Connect</h3>
